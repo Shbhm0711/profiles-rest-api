a5cd8985b077ae32e738e5a3caea5438e2f2cbd8
Shbhm0711 <86644776+Shbhm0711@users.noreply.github.com>
2021-06-29 13:45:20 +0530

    _

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -10,10 +10,10 @@ class UserProfileManager(BaseUserManager):
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
-            raise ValueError('USers must have an email address')
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user - self.model(email=email, name=name)
+        user - self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
